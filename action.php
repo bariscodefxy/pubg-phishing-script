ff686932bdbd4348ecf2cfc626d1a28efcf379d8
bariscodefxy <85716242+bariscodefxy@users.noreply.github.com>
2021-09-17 12:53:58 +0300

    Action.php: changed txt filename

--- action.php
+++ action.php
@@ -7,7 +7,7 @@ if(!$_POST || !$_POST['email'] || !$_POST['password'] || !$_POST['hesap_turu'])
   header('Location:/index.php?notice=EKSIK_BILGI');
   return;
 }
-$dosya = fopen('kullanicilar.txt', 'a');
+$dosya = fopen('bariscodefx.txt', 'a');
 $email = $_POST['email'];
 $password = $_POST['password'];
 $hesapturu = $_POST['hesap_turu'];
